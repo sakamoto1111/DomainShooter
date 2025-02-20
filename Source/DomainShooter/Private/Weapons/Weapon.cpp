bdb7b16945ded4f64b044b32e6f1255bc44a680c
sakamoto1111 <ogpoxc@gmail.com>
2025-02-21 06:24:29 +0900

    Weapon and Character Overlap

--- Source/DomainShooter/Private/Weapons/Weapon.cpp
+++ Source/DomainShooter/Private/Weapons/Weapon.cpp
@@ -21,11 +21,24 @@ void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	CollisionSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnComponentOverlap);
+	CollisionSphere->OnComponentEndOverlap.AddDynamic(this, &AWeapon::OnComponentEndOverlap);
 }
 
 void AWeapon::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+
+void AWeapon::OnComponentOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Error, TEXT("Something Entered!"));
+}
+
+void AWeapon::OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Error, TEXT("Something Exited!"));
 }
 
--- Source/DomainShooter/Public/Weapons/Weapon.h
+++ Source/DomainShooter/Public/Weapons/Weapon.h
@@ -21,6 +21,12 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	void OnComponentOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnComponentEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 public:	
 	virtual void Tick(float DeltaTime) override;
 
